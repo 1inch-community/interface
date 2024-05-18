71dec530996a6377576b12f019814b0d444dceb7
DKotsyuba <DKotsyuba@users.noreply.github.com>
2024-05-18 18:38:00 +0000

    deploy: ea57c38768e36b569f729c55b4036e8e5916c2e0

--- assets/one-inch.repository-YYH7S46H-Kx5IEGc0.js
+++
@@ -1,2 +0,0 @@
-import{aa as a}from"./index-nML9Z3oK.js";var s=({address:n,chainId:e})=>new Promise((t,r)=>{if(!n||e!==a.eth)return r();const o=new Image;o.onload=()=>t(o),o.onerror=r,o.src=`https://tokens-data.1inch.io/images/${n.toLowerCase()}.png`});export{s as oneInchRepository};
-//# sourceMappingURL=one-inch.repository-YYH7S46H-Kx5IEGc0.js.map
