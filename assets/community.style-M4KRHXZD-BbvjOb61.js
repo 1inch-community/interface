7d6d2891fd4e62a664a182ffd58e0945d194deeb
DKotsyuba <DKotsyuba@users.noreply.github.com>
2024-05-18 16:52:08 +0000

    deploy: 4c75fe49392fe6d0600db985db37063e78d35caa

---
+++ assets/community.style-M4KRHXZD-BbvjOb61.js
@@ -0,0 +1,12 @@
+import{i as r}from"./index-Bgau2dAd.js";var a=r`
+
+  :root {
+      --primary: #00a0b5;
+      --primary-50: rgb(0 160 181 / 50%);
+      --primary-hover: #007e8f;
+      --secondary: #00a0b51f;
+      --secondary-hover: #00a0b53d;
+  }
+  
+`;export{a as communityStyle};
+//# sourceMappingURL=community.style-M4KRHXZD-BbvjOb61.js.map
---
+++ assets/native-DCTjMhwz.js
@@ -0,0 +1,2 @@
+var e={};function t(){if(typeof WebSocket<"u")return WebSocket;if(typeof e.WebSocket<"u")return e.WebSocket;if(typeof window.WebSocket<"u")return window.WebSocket;if(typeof self.WebSocket<"u")return self.WebSocket;throw new Error("`WebSocket` is not supported in this environment")}const o=t();export{o as WebSocket};
+//# sourceMappingURL=native-DCTjMhwz.js.map
