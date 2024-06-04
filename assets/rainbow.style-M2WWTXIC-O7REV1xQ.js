2d3f9df5aa09f3d79fb790c532b6fbc1e5ee2937
DKotsyuba <DKotsyuba@users.noreply.github.com>
2024-06-04 10:24:58 +0000

    deploy: 2efdc4bf2afa3a204372da70e9e848793c0d2965

---
+++ assets/rainbow.style-M2WWTXIC-O7REV1xQ.js
@@ -0,0 +1,2 @@
+import{m as o,r}from"./index-CiPMkqQ5.js";var m=o(r[0]);export{m as rainbowStyle};
+//# sourceMappingURL=rainbow.style-M2WWTXIC-O7REV1xQ.js.map
---
+++ assets/violet.style-LYRK4XDV-B--SWReE.js
@@ -0,0 +1,2 @@
+import{m as o}from"./index-CiPMkqQ5.js";var e=o("#2f8af5");export{e as violetStyle};
+//# sourceMappingURL=violet.style-LYRK4XDV-B--SWReE.js.map
