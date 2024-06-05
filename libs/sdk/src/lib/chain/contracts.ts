8c09f8689e3c261691e4688f9ba7f00db99164e3
DenisKotsyuba <kocubads96@gmail.com>
2024-06-05 16:58:27 +0300

    feat(sdk): added router v6 contract addresses

--- libs/sdk/src/lib/chain/contracts.ts
+++ libs/sdk/src/lib/chain/contracts.ts
@@ -1,3 +1,22 @@
-export enum Contracts {
+import { ChainId } from '@one-inch-community/models';
+import { Address } from 'viem';
+import { requireChainId } from './is-chain-id';
 
+const oneInchRouterV6: Record<ChainId, Address> = {
+  [ChainId.eth]: '0x111111125421ca6dc452d289314280a0f8842a65',
+  [ChainId.bnb]: '0x111111125421ca6dc452d289314280a0f8842a65',
+  [ChainId.matic]: '0x111111125421ca6dc452d289314280a0f8842a65',
+  [ChainId.op]: '0x111111125421ca6dc452d289314280a0f8842a65',
+  [ChainId.arbitrum]: '0x111111125421ca6dc452d289314280a0f8842a65',
+  [ChainId.gnosis]: '0x111111125421ca6dc452d289314280a0f8842a65',
+  [ChainId.avalanche]: '0x111111125421ca6dc452d289314280a0f8842a65',
+  [ChainId.fantom]: '0x111111125421ca6dc452d289314280a0f8842a65',
+  [ChainId.aurora]: '0x111111125421ca6dc452d289314280a0f8842a65',
+  [ChainId.klaytn]: '0x111111125421ca6dc452d289314280a0f8842a65',
+  [ChainId.zkSyncEra]: '0x6fd4383cb451173d5f9304f041c7bcbf27d561ff'
+}
+
+export function getOneInchRouterV6ContractAddress(chainId: ChainId): Address {
+  requireChainId(chainId)
+  return oneInchRouterV6[chainId]
 }
--- libs/sdk/src/lib/chain/is-chain-id.ts
+++ libs/sdk/src/lib/chain/is-chain-id.ts
@@ -3,3 +3,7 @@ import { ChainId } from "@one-inch-community/models";
 export function isChainId(chainId: any): chainId is ChainId {
   return ChainId[chainId] !== undefined
 }
+
+export function requireChainId(chainId: any): void | never {
+  if (!isChainId(chainId)) throw new Error(`chain ${chainId} not supported`)
+}
