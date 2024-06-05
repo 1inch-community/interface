f389b29c7884b4117aa80e8fb51a5ff81d63bf65
DenisKotsyuba <kocubads96@gmail.com>
2024-06-05 11:00:47 +0300

    fix(lit): fixed memory leak

--- libs/lit/src/lib/directives/observe-directive.ts
+++ libs/lit/src/lib/directives/observe-directive.ts
@@ -4,10 +4,17 @@ import { asapScheduler, Observable, observeOn, Subscription } from 'rxjs';
 
 class ObserveDirective<T> extends AsyncDirective {
   private subscription?: Subscription;
+  private lastValue?: T;
 
-  override render(observable: Observable<T>, initValue?: T) {
-    this.subscription = observable.pipe(observeOn(asapScheduler)).subscribe(value => this.setValue(value));
-    return initValue ?? '';
+  override render(observable: Observable<T>, initValue?: T): T {
+    if (this.subscription) {
+      return this.lastValue ?? initValue ?? ('' as T)
+    }
+    this.subscription = observable.pipe(observeOn(asapScheduler)).subscribe(value => {
+      this.lastValue = value
+      this.setValue(value)
+    });
+    return initValue ?? ('' as T);
   }
 
   override disconnected() {
