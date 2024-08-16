ea5ba6908d98e05dbc3f68106c8f0e5cdbed480e
DenisKotsyuba <kocubads96@gmail.com>
2024-08-16 13:45:27 +0300

    feat: Add initial Vue.js example project
    This commit sets up a Vue.js example project, including core configuration files and dependencies. Added project-specific files such as `package.json`, `.npmrc`, and various configurations necessary for the project's build and development environments. Also included assets like favicon and logo for the project.

---
+++ examples/vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <HelloWorld/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+});
+</script>
+
+<style>
+</style>
